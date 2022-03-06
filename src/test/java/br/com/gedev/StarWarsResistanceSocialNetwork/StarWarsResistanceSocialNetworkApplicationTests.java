d5906932aa155ec57e9d651ea2567cb2e45e581f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-06 08:43:30 -0300

    initial commit

---
+++ src/test/java/br/com/gedev/StarWarsResistanceSocialNetwork/StarWarsResistanceSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gedev.StarWarsResistanceSocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsResistanceSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
