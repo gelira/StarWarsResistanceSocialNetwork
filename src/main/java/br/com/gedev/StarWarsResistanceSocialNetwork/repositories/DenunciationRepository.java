afc7315a28b671e4c07d5830561131a45f53de40
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-11 14:33:04 -0300

    custom queries

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/DenunciationRepository.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/DenunciationRepository.java
@@ -1,7 +1,16 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.repositories;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Denunciation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface DenunciationRepository extends CrudRepository<Denunciation, Long> {
+    @Query("select d from Denunciation d " +
+            "where d.accuserRebel.id = :accuser_rebel_id and d.accusedRebel.id = :accused_rebel_id")
+    Optional<Denunciation> findByAccuserAndAccused(
+            @Param("accuser_rebel_id") Long accuserRebelId,
+            @Param("accused_rebel_id") Long accusedRebelId);
 }
--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/LocationRepository.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/LocationRepository.java
@@ -1,7 +1,20 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.repositories;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Location;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface LocationRepository extends CrudRepository<Location, Long> {
+    @Modifying
+    @Query("update Location l " +
+            "set l.current = false, l.updatedAt = CURRENT_TIMESTAMP " +
+            "where l.rebel.id = :rebel_id and l.current = true")
+    void updateLocationsCurrentToFalse(@Param("rebel_id") Long rebelId);
+
+    @Query("select l from Location l where l.rebel.id = :rebel_id and l.current = true")
+    Optional<Location> findCurrentLocation(@Param("rebel_id") Long rebelId);
 }
