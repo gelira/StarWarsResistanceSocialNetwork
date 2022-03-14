30a6531749d8137c3db1ca8e040f5ac5772e0a6c
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-14 13:58:23 -0300

    creating rebel with locations

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/RebelController.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/RebelController.java
@@ -2,8 +2,6 @@ package br.com.gedev.StarWarsResistanceSocialNetwork.controllers;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateRebelDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.RebelDTO;
-import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
-import br.com.gedev.StarWarsResistanceSocialNetwork.mappers.RebelMapper;
 import br.com.gedev.StarWarsResistanceSocialNetwork.services.RebelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +16,9 @@ import javax.validation.Valid;
 @RequestMapping("/rebels")
 public class RebelController {
     private final RebelService rebelService;
-    private final RebelMapper rebelMapper;
 
     @PostMapping
     public RebelDTO createRebel(@Valid @RequestBody CreateRebelDTO createRebelDTO) {
-        Rebel rebel = rebelService.createRebel(
-                rebelMapper.fromCreateDTOToEntity(createRebelDTO));
-        return rebelMapper.fromEntityToRebelDTO(rebel);
+        return rebelService.createRebel(createRebelDTO);
     }
 }
--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/services/RebelService.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/services/RebelService.java
@@ -1,16 +1,32 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.services;
 
+import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateRebelDTO;
+import br.com.gedev.StarWarsResistanceSocialNetwork.dto.LocationDTO;
+import br.com.gedev.StarWarsResistanceSocialNetwork.dto.RebelDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
+import br.com.gedev.StarWarsResistanceSocialNetwork.mappers.RebelMapper;
 import br.com.gedev.StarWarsResistanceSocialNetwork.repositories.RebelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class RebelService {
     private final RebelRepository rebelRepository;
+    private final LocationService locationService;
+    private final RebelMapper rebelMapper;
 
-    public Rebel createRebel(Rebel rebel) {
-        return rebelRepository.save(rebel);
+    @Transactional
+    public RebelDTO createRebel(CreateRebelDTO createRebelDTO) {
+        Rebel toCreate = rebelMapper.fromCreateDTOToEntity(createRebelDTO);
+        Rebel created = rebelRepository.save(toCreate);
+
+        LocationDTO locationDTO = locationService.createLocation(createRebelDTO.getLocation(), created);
+
+        RebelDTO dto = rebelMapper.fromEntityToRebelDTO(created);
+        dto.setLocation(locationDTO);
+
+        return dto;
     }
 }
