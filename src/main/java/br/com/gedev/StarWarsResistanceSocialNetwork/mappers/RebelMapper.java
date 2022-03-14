5d7e2d85eed7d78f1e8fb00e30b1a11d009992e8
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-14 17:24:17 -0300

    improve rebel mapper

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/mappers/RebelMapper.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/mappers/RebelMapper.java
@@ -2,14 +2,11 @@ package br.com.gedev.StarWarsResistanceSocialNetwork.mappers;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateRebelDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.RebelDTO;
-import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Location;
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
 import br.com.gedev.StarWarsResistanceSocialNetwork.services.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
@@ -24,10 +21,8 @@ public class RebelMapper {
 
     public RebelDTO fromEntityToRebelDTO(Rebel entity) {
         RebelDTO dto = modelMapper.map(entity, RebelDTO.class);
-
-        Optional<Location> locationOptional = locationService.findCurrentLocation(entity);
-        locationOptional.ifPresent(location -> dto.setLocation(locationMapper.fromEntityToDTO(location)));
-
+        locationService.findCurrentLocation(entity).ifPresent(
+                location -> dto.setLocation(locationMapper.fromEntityToDTO(location)));
         return dto;
     }
 }
