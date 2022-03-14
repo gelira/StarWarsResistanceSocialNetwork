4f89935c58e102ed76a8febdbdbcda18c561964c
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-14 13:57:30 -0300

    update locations and set rebel

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/services/LocationService.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/services/LocationService.java
@@ -3,6 +3,7 @@ package br.com.gedev.StarWarsResistanceSocialNetwork.services;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateLocationDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.LocationDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Location;
+import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
 import br.com.gedev.StarWarsResistanceSocialNetwork.mappers.LocationMapper;
 import br.com.gedev.StarWarsResistanceSocialNetwork.repositories.LocationRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,12 @@ public class LocationService {
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
 
-    public LocationDTO createLocation(CreateLocationDTO createLocationDTO) {
+    public LocationDTO createLocation(CreateLocationDTO createLocationDTO, Rebel rebel) {
+        locationRepository.updateLocationsCurrentToFalse(rebel.getId());
+
         Location toCreate = locationMapper.fromCreateDTOToEntity(createLocationDTO);
+        toCreate.setRebel(rebel);
+
         Location created = locationRepository.save(toCreate);
         return locationMapper.fromEntityToDTO(created);
     }
