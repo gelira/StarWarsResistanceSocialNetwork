387f8078049f15abc30fb9c02cf9da9f7d677136
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-14 22:01:58 -0300

    query find rebel by uuid

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/RebelRepository.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/RebelRepository.java
@@ -1,7 +1,14 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.repositories;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface RebelRepository extends CrudRepository<Rebel, Long> {
+    @Query("select r from Rebel r where r._id = :rebel_id")
+    Optional<Rebel> findByUUID(@Param("rebel_id") UUID rebelId);
 }
