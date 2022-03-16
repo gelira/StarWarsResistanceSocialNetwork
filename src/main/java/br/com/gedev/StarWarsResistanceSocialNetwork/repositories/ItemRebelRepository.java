b79cca5120013b7e2e8d8a6bc2c707e9fb4bfa77
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-16 16:49:00 -0300

    remove useless query items rebel

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/ItemRebelRepository.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/repositories/ItemRebelRepository.java
@@ -5,13 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ItemRebelRepository extends CrudRepository<ItemRebel, Long> {
     @Query("select i from ItemRebel i where i.rebel.id = :rebel_id and i.item.id = :item_id")
     Optional<ItemRebel> findByRebelAndItem(@Param("rebel_id") Long rebelId, @Param("item_id") Long itemId);
-
-    @Query("select i from ItemRebel i where i.rebel.id = :rebel_id")
-    List<ItemRebel> findByRebelId(@Param("rebel_id") Long rebelI);
 }
