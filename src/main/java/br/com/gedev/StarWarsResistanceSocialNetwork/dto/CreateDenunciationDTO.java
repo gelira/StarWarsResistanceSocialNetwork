ec44fda2edaf95db7200d6f5280cf59be73b7a88
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-17 12:52:40 -0300

    fix not null validation create denunciation dto

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/dto/CreateDenunciationDTO.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/dto/CreateDenunciationDTO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
@@ -12,8 +13,10 @@ import java.util.UUID;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateDenunciationDTO {
     @JsonProperty("accuser_rebel_id")
+    @NotNull
     private UUID accuserRebelId;
 
     @JsonProperty("accused_rebel_id")
+    @NotNull
     private UUID accusedRebelId;
 }
