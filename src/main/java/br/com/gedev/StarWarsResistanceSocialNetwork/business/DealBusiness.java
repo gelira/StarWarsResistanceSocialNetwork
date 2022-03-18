88389840ef44bef3487e3c873b0178e1913a55fb
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-18 09:03:31 -0300

    create deal in business class

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/business/DealBusiness.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/business/DealBusiness.java
@@ -1,20 +1,46 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.business;
 
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateDealDTO;
+import br.com.gedev.StarWarsResistanceSocialNetwork.dto.DealDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Deal;
+import br.com.gedev.StarWarsResistanceSocialNetwork.entities.DealItem;
 import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
-import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.DealRebelTraitorException;
-import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.RebelNotFoundException;
+import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.*;
+import br.com.gedev.StarWarsResistanceSocialNetwork.mappers.DealMapper;
+import br.com.gedev.StarWarsResistanceSocialNetwork.services.DealService;
 import br.com.gedev.StarWarsResistanceSocialNetwork.services.RebelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
 public class DealBusiness {
     private final RebelService rebelService;
+    private final DealService dealService;
+    private final DealMapper dealMapper;
+    private final DealItemBusiness dealItemBusiness;
+
+    @Transactional
+    public DealDTO createDeal(CreateDealDTO createDealDTO)
+            throws DealRebelTraitorException, RebelNotFoundException, InsufficientItemsException,
+            InvalidItemIdException, DealItemPointsNotEquivalentException {
+
+        Deal dealToCreate = validateDeal(createDealDTO);
+
+        List<DealItem> dealItemsRebel1 = dealItemBusiness.validateAndAggregateDealItems(
+                createDealDTO.getRebel1Items(), dealToCreate.getRebel1(), dealToCreate.getRebel2());
+        List<DealItem> dealItemsRebel2 = dealItemBusiness.validateAndAggregateDealItems(
+                createDealDTO.getRebel2Items(), dealToCreate.getRebel2(), dealToCreate.getRebel1());
+
+        dealItemBusiness.validateItemPoints(dealItemsRebel1, dealItemsRebel2);
+
+        Deal dealCreated = dealService.createDeal(dealToCreate, dealItemsRebel1, dealItemsRebel2);
+        return dealMapper.fromEntityToDTO(dealCreated);
+    }
 
     public Deal validateDeal(CreateDealDTO createDealDTO) throws RebelNotFoundException, DealRebelTraitorException {
         Deal entity = new Deal();
