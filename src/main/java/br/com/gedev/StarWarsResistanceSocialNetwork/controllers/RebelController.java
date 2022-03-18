1eabd5d40ac5765a65cb810a37e0bf149a082d6b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-18 08:48:37 -0300

    location business

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/RebelController.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/RebelController.java
@@ -1,14 +1,10 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.controllers;
 
+import br.com.gedev.StarWarsResistanceSocialNetwork.business.LocationBusiness;
 import br.com.gedev.StarWarsResistanceSocialNetwork.business.RebelBusiness;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.*;
-import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Location;
-import br.com.gedev.StarWarsResistanceSocialNetwork.entities.Rebel;
 import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.InvalidItemIdException;
 import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.RebelNotFoundException;
-import br.com.gedev.StarWarsResistanceSocialNetwork.mappers.LocationMapper;
-import br.com.gedev.StarWarsResistanceSocialNetwork.services.LocationService;
-import br.com.gedev.StarWarsResistanceSocialNetwork.services.RebelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +15,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/rebels")
 public class RebelController {
-    private final RebelService rebelService;
-    private final LocationService locationService;
-    private final LocationMapper locationMapper;
     private final RebelBusiness rebelBusiness;
+    private final LocationBusiness locationBusiness;
 
     @PostMapping
     public RebelDTO createRebel(@Valid @RequestBody CreateRebelDTO createRebelDTO) throws InvalidItemIdException {
@@ -31,13 +25,9 @@ public class RebelController {
 
     @PostMapping("{id}/locations")
     public LocationDTO createLocation(
-            @PathVariable("id") UUID rebelId,
-            @Valid @RequestBody CreateLocationDTO createLocationDTO) throws RebelNotFoundException {
+            @PathVariable("id") UUID rebelId, @Valid @RequestBody CreateLocationDTO createLocationDTO)
+            throws RebelNotFoundException {
 
-        Rebel rebel = rebelService.findRebelByUUID(rebelId);
-        Location locationToCreate = locationMapper.fromCreateDTOToEntity(createLocationDTO);
-        Location locationCreated = locationService.createLocation(locationToCreate, rebel);
-
-        return locationMapper.fromEntityToDTO(locationCreated);
+        return locationBusiness.createNewLocation(rebelId, createLocationDTO);
     }
 }
