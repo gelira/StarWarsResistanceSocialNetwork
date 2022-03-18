26a84b73bce036c779bbe35ce0e2de417940b5be
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-17 22:15:37 -0300

    create deal dto

---
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/dto/CreateDealDTO.java
@@ -0,0 +1,33 @@
+package br.com.gedev.StarWarsResistanceSocialNetwork.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreateDealDTO {
+    @JsonProperty("rebel1_id")
+    @NotNull
+    private UUID rebel1Id;
+
+    @JsonProperty("rebel2_id")
+    @NotNull
+    private UUID rebel2Id;
+
+    @NotEmpty
+    @NotNull
+    private List<@Valid CreateItemDealDTO> rebel1Items;
+
+    @NotEmpty
+    @NotNull
+    private List<@Valid CreateItemDealDTO> rebel2Items;
+}
