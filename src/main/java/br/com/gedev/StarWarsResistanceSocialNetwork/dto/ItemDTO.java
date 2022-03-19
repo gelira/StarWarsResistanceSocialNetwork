a47b235413aa6e1db12c7e71114c85ffe2f936f6
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-19 11:15:51 -0300

    change item dto

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/dto/ItemDTO.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/dto/ItemDTO.java
@@ -1,8 +1,10 @@
 package br.com.gedev.StarWarsResistanceSocialNetwork.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Getter
@@ -10,4 +12,11 @@ import java.util.UUID;
 public class ItemDTO {
     private UUID _id;
     private String name;
+    private Double value;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+
+    @JsonProperty("updated_at")
+    private Date updatedAt;
 }
