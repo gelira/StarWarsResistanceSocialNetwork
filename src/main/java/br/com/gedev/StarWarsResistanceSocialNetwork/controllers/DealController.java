f2f389818b3e0620f3f79f90f840289f2dae7f21
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-03-19 11:45:12 -0300

    ordering list deals

--- src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/DealController.java
+++ src/main/java/br/com/gedev/StarWarsResistanceSocialNetwork/controllers/DealController.java
@@ -5,18 +5,21 @@ import br.com.gedev.StarWarsResistanceSocialNetwork.dto.CreateDealDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.dto.DealDTO;
 import br.com.gedev.StarWarsResistanceSocialNetwork.exceptions.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/deals")
 public class DealController {
     private final DealBusiness dealBusiness;
+
+    @GetMapping
+    public List<DealDTO> listAllDeals() {
+        return dealBusiness.listAllDeals();
+    }
 
     @PostMapping
     public DealDTO createDeal(@Valid @RequestBody CreateDealDTO createDealDTO)
